7fb51c21a58b528c931901e340c651d86471a1c7
DimBerdekas <33751225+tsanmi@users.noreply.github.com>
2020-07-28 20:24:07 +0300

    Update TwitterCrawler.java

--- Twitter-Crawler/src/twitter/crawler/TwitterCrawler.java
+++ Twitter-Crawler/src/twitter/crawler/TwitterCrawler.java
@@ -24,10 +24,10 @@ public class TwitterCrawler {
   
     public static void main(String[] args) {
          ConfigurationBuilder cf=new ConfigurationBuilder();
-    cf.setDebugEnabled(true).setOAuthConsumerKey("SgWka3I6DTkjo5O3frJPztcrN").
-            setOAuthConsumerSecret("cd7ZAKbvwAYZMKdVrafLTEFft33eZ4EKXi245hu6yFgOIarqIf")
-            .setOAuthAccessToken("1946495047-dWuXKxgm8lx3qYtxiNLU5KO9LKuuIgsfvFyqH3A")
-            .setOAuthAccessTokenSecret("xRlJN7amSChUv318AigXL5E1r04yVdVkXDO8nNgvJspPe");
+    cf.setDebugEnabled(true).setOAuthConsumerKey("xxxxx").
+            setOAuthConsumerSecret("xxxxx")
+            .setOAuthAccessToken("1946495047-xxxxx")
+            .setOAuthAccessTokenSecret("xxxxx");
             TwitterFactory tf=new TwitterFactory(cf.build());
             twitter4j.Twitter twitter=tf.getInstance(); 
             ArrayList<Status> retweet=new ArrayList<Status>();
